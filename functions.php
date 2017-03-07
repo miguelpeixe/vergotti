4b88783301785b78fde4a5c2c936104f19015a44
miguelpeixe <miguel@cardume.art.br>
2017-03-07 14:30:21 -0300

    enqueue parent styles

--- functions.php
+++ functions.php
@@ -3,4 +3,4 @@
 function vergotti_scripts() {
   wp_enqueue_style('twentyfifteen-main-styles', get_template_directory_uri() . '/style.css');
 }
-add_action('wp_enqueue_styles', 'vergotti_styles');
+add_action('wp_enqueue_scripts', 'vergotti_scripts');
